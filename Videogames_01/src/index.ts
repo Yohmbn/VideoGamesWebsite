a7d5facd064e607fd3debbd38b4b55b71e9cd50d
Yohan M <inuyasha59260@gmail.com>
2022-02-17 14:48:13 +0100

    Videogames_01

--- Videogames_01/src/index.ts
+++ Videogames_01/src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import nunjucks from "nunjucks";
-import { getGames } from "./getGames";
-import * as readline from "readline";
+import request from "@fewlines-education/request";
 
 const app = express();
 
@@ -10,23 +9,74 @@ nunjucks.configure("views", {
   express: app,
 });
 
-const reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+app.set("view engine", "njk");
+
+app.use(express.static("public"));
+
+app.get("/", (httpRequest, response) => {
+  request("http://videogame-api.fly.dev/platforms", (error, body) => {
+    if (error) {
+      throw error;
+    }
+    const platform = JSON.parse(body).platforms;
+    response.render("home", { platform });
+  });
 });
 
-app.get("/", (request, response) => {
-  response.render("home"); // 🔎 See? "home"!
+type Logo = {
+  url: string;
+  width: number;
+  height: number;
+};
+
+type PlatformInfo = {
+  id: string;
+  name: string;
+  slug: string;
+  category: string;
+  summary: string;
+  logo: Logo;
+};
+
+app.get("/platform/:id", (httpRequest, response) => {
+  const id = httpRequest.params.id;
+  console.log(id);
+
+  request("http://videogame-api.fly.dev/platforms", (error, body) => {
+    if (error) {
+      throw error;
+    }
+    const platforms = JSON.parse(body).platforms;
+    const myPlatform = platforms.find((element: PlatformInfo) => {
+      return element.id === id;
+    });
+
+    request(`https://videogame-api.fly.dev/games/platforms/${id}`, (error, body) => {
+      if (error) {
+        throw error;
+      }
+      const games = JSON.parse(body);
+      response.render("games", { games, myPlatform });
+    });
+  });
 });
 
-app.listen(2000, () => {
-  console.log("Server started on http://localhost:2000");
+app.get("/plateform/:platform_id/games/:game_id", (httprequest, response) => {
+  const platform_id = httprequest.params.platform_id;
+  const game_id = httprequest.params.game_id;
+  console.log(platform_id);
+  console.log(game_id);
+
+  request(`http://videogame-api.fly.dev/games/${game_id}`, (error, body) => {
+    if (error) {
+      throw error;
+    }
+    const gameInfo = JSON.parse(body);
+    console.log(gameInfo);
+    response.render("gameInfo", { gameInfo });
+  });
 });
 
-getGames();
-
-// app.set("view engine", "njk");
-
-// app.get("/", (request, response) => {
-//   response.render("home"); // 🔎 See? "home"!
-// });
+app.listen(1002, () => {
+  console.log("Server started on http://localhost:1002");
+});
